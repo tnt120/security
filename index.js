c669e3ba8b2793501ecf74024f11e08ecf4c78ad
tnt120 <48412587+tnt120@users.noreply.github.com>
2020-06-02 12:30:52 +0200

    Files

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 const file = require('./test.json')
 
 app.use(cors());
+app.use(bodyParser());
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.end(JSON.stringify(req.body));
+})
 
 const items = file;
 app.get("/", (req, res) => {
